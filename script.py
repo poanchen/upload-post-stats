298ce1ae20945499e65d8b61d550d63bd045179b
PoAn (Baron) Chen <chen.baron@hotmail.com>
2020-06-17 21:15:11 -0700

    typo

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import requests, datetime
 print 'Beginning to get the XML file...'
 print 'Starting time: %s' % datetime.datetime.now()
 r = requests.get(config['xml_url'])
-print 'Finished to get the XML file'
+print 'Finished to get the XML file.'
 print 'Ending time: %s' % datetime.datetime.now()
 
 # Extract relative blog posts link from the XML
@@ -18,12 +18,12 @@ for child in root:
   match = re.match('https://poanchen\.github\.io(/blog/[0-9]{4}/[0-9]{2}/[0-9]{2}/.+)', child[0].text)
   if match:
     links.append(match.group(1))
-print 'Finished to extract relative blog posts link from the XML file'
+print 'Finished to extract relative blog posts link from the XML file.'
 print 'Ending time: %s' % datetime.datetime.now()
 
 # Get the all time views for all the posts from Google's Core Reporting API
 import helper
-print 'Beginning to extract relative blog posts link from the XML file...'
+print 'Beginning to get the all time views for each individual blog posts...'
 print 'Starting time: %s' % datetime.datetime.now()
 core_reporting_api_service = helper.get_core_reporting_api_service()
 data = {}
@@ -34,7 +34,7 @@ for link in links:
                 end_date=config['views_end_date'] ,
                 metrics='ga:pageviews',
                 filters='ga:pagepath==%s' % (link)).execute().get('totalsForAllResults').get('ga:pageviews')
-print 'Finished to extract relative blog posts link from the XML file...'
+print 'Finished to get the all time views for each individual blog posts.'
 print 'Ending time: %s' % datetime.datetime.now()
 
 # Convert dict to JSON file
@@ -42,7 +42,7 @@ import json
 print 'Beginning to convert dict to JSON file...'
 print 'Starting time: %s' % datetime.datetime.now()
 open(config['json_file_name'], 'wb').write(json.dumps(data))
-print 'Finished to convert dict to JSON file'
+print 'Finished to convert dict to JSON file.'
 print 'Ending time: %s' % datetime.datetime.now()
 
 # Upload it to the Azure Blob Storage
@@ -56,5 +56,5 @@ blob_service.create_blob_from_path(
   config['container_name'],
   config['json_file_name'],
   full_path_to_file)
-print 'Finished to upload the JSON file'
+print 'Finished to upload the JSON file.'
 print 'Ending time: %s' % datetime.datetime.now()
